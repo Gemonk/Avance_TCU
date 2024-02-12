6032e33727cd21189f4194ad8e855859e0d7ea7f
Cipher <72933078+DGM9293@users.noreply.github.com>
2024-02-12 09:53:52 -0600

    Cambio en el funcionamiento del jugador

--- Proyecto_TCU/TCUGame/Personajes/player.gd
+++ Proyecto_TCU/TCUGame/Personajes/player.gd
@@ -1,44 +1,27 @@
 extends CharacterBody2D
 
-var speed := 200
+var speed := 120
 var direccion := 0.0
 var jump := 250
-const gravity := 0
+const gravity := 8
 
 @onready var animacion := $AnimationPlayer
 @onready var sprite := $Sprite2D
 func _physics_process(delta):
-	animacion.play("idle")
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-		velocity.y = 0
+	direccion = Input.get_axis("ui_left","ui_right")
+	velocity.x = direccion * speed
+	
+	if direccion != 0:
 		animacion.play("walk")
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		velocity.y = 0
-		animacion.play("walk")
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -speed
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = speed
-		velocity.x = 0
 	else:
-		velocity.y = 0
-		velocity.x = 0
-		
-			
-	#if direccion != 0:
-	#	animacion.play("walk")
-	#else:
-	#	animacion.play("idle")
+		animacion.play("idle")
 	
-	#sprite.flip_h = direccion < 0 if direccion != 0 else sprite.flip_h
+	sprite.flip_h = direccion < 0 if direccion != 0 else sprite.flip_h
 	
-	#if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
-	#	velocity.y -= jump
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y -= jump
 		
-	#if !is_on_floor():
-	#	velocity.y += gravity
+	if !is_on_floor():
+		velocity.y += gravity
 	
 	move_and_slide()
